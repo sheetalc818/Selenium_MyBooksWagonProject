3c4745ac2ef107449655ce387b2170ecd50863b7
sheetalc818 <sheetalc818@gmail.com>
2020-07-09 17:14:18 +0530

    REFACTORED :BaseTest.class file

--- src/main/java/com/bridgelabz/bookswagon/base/BaseTest.java
+++ src/main/java/com/bridgelabz/bookswagon/base/BaseTest.java
@@ -2,9 +2,11 @@ package com.bridgelabz.bookswagon.base;
 
 import java.io.IOException;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.bridgelabz.bookswagon.util.IAutoConstant;
 import com.bridgelabz.bookswagon.util.ReadingPropertiseFile;
@@ -17,20 +19,21 @@ public class BaseTest implements IAutoConstant
 	
 	public static void initialize() throws InterruptedException, IOException 
 	{
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("test-type");
-		options.addArguments("start-maximized");
-		options.addArguments("--window-size=1920,1080");
-		options.addArguments("--enable-precise-memory-info");
-		options.addArguments("--disable-popup-blocking");
-		options.addArguments("--disable-default-apps");
-		options.addArguments("test-type=browser");
-		options.addArguments("--no-sandbox");
-		options.addArguments("--headless");
-		options.addArguments("--disable-dev-shm-usage");
+		WebDriverManager.firefoxdriver().setup();
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("test-type");
+//		options.addArguments("start-maximized");
+//		options.addArguments("--window-size=1920,1080");
+//		options.addArguments("--enable-precise-memory-info");
+//		options.addArguments("--disable-popup-blocking");
+//		options.addArguments("--disable-default-apps");
+//		options.addArguments("test-type=browser");
+//		options.addArguments("--no-sandbox");
+//		options.addArguments("--headless");
+//		options.addArguments("--disable-dev-shm-usage");
 
-		driver=new ChromeDriver(options);
+		//driver=new ChromeDriver(options);
+		driver= new FirefoxDriver();
 		driver.manage().window().maximize();
 		String appUrl = ReadingPropertiseFile.getProperty(PROPERTY_FILE_PATH,"URL");
 		driver.get(appUrl);
